3b7ce51f3cb2824064f8597adf4dff03b1cb8a25
Riley Mummah <rmummah@umass.edu>
2024-01-26 08:44:07 -0500

    file rearrangement
    Merge branch 'main' of https://github.com/rileymummah/BirdSDM
    
    # Conflicts:
    #	code/cawa1.R

---
+++ HPC/cawa1.R
@@ -0,0 +1,168 @@
+
+## This code was written by: fiona lunt
+
+## Objective ---------------------------
+## 
+## Canada Warbler models 1-7
+##
+## Input:
+##   
+##
+## Output: 
+##
+##
+
+## load packages ---------------------------
+# library(tidyverse)
+library(plyr)
+library(dplyr)
+library(tibble)
+library(magrittr)
+library(mgcv)
+library(jagsUI)
+library(ROCR)
+
+path = '/caldera/hovenweep/projects/usgs/ecosystems/eesc/rmummah/proj05-fiona/'
+
+## load functions ---------------------------
+# Generate code for GAMs
+generate.code <- function(dat) {
+  jagam(cawadet ~ s(elev, k=10) + s(slope, k=10) + #s(temp, k=10) +
+          s(ppt, k=10) + s(can, k=10) + s(dev, k=10) + #s(forest, k=10) +
+          s(road, k=10) + s(longitude, latitude, bs='ds', k=100),
+        data = dat, family='binomial', file = paste0(path,'placeholder.txt'))
+}
+
+## load data ---------------------------
+test1 <- read.csv(paste0(path,"data/test1.csv"))
+train1 <- read.csv(paste0(path,"data/train1.csv"))
+
+dat1 <- rbind.fill(train1, test1)
+
+
+# Model 1: R1 - CAWA ------------------------------------------------------
+#train: eBird- 1:19829, BBA- 19830:46832, BBS- 46833:79509
+train.eBird = 1:19829
+train.BBA = 19830:46832
+train.BBS = 46833:79509
+#test: eBird- 79510:88726, BBA- 88727:95456, BBS- 95457:103729
+test.eBird = 79510:88726
+test.BBA = 88727:95456
+test.BBS = 95457:103729
+
+
+m1 <- generate.code(dat1)
+
+# Use the output found in placeholder.txt in the JAGS model file: cawam1.txt
+
+datm1 <- list(y = c(dat1$cawadet[train.eBird],
+                    dat1$cawatot[train.BBA],
+                    dat1$cawadet[train.BBS]),
+              X = m1$jags.data$X, n = m1$jags.data$n, zero = m1$jags.data$zero,
+              S1 = m1$jags.data$S1, S2 = m1$jags.data$S2, S3 = m1$jags.data$S3,
+              S4 = m1$jags.data$S4, S5 = m1$jags.data$S5, S6 = m1$jags.data$S6,
+              S7 = m1$jags.data$S7, #S8 = m1$jags.data$S8, S9 = m1$jags.data$S9,
+              ehours = dat1$duration_minutes[train.eBird],
+              ekm = dat1$effort_distance_km[train.eBird],
+              ehours2 = dat1$duration_minutes[test.eBird],
+              ekm2 = dat1$effort_distance_km[test.eBird],
+              hsm = dat1$hsm[train.BBA],
+              hsm2 = dat1$hsm[test.BBA],
+              doy = dat1$doy[train.BBA],
+              doy2 = dat1$doy[test.BBA],
+              doy3 = dat1$doy[train.BBS],
+              doy4 = dat1$doy[test.BBS])
+
+initsm1 <- function(){
+  list(b = m1$jags.ini$b,
+       lambda = m1$jags.ini$lambda,
+       beta = rep(3, 0.001),
+       z = as.numeric(datm1$y>0))
+}
+
+outm1 <- jags(data = datm1,
+              parameters.to.save = c("beta","b","y2"),
+              inits = initsm1,
+              model.file = paste0(path,"models/cawam1.txt"),
+              n.chains = 3,
+              n.thin = 2,
+              n.adapt = 500,
+              n.burnin = 500,
+              n.iter = 2500,
+              parallel = TRUE)
+
+### Performance metrics -----------------------------------------------------
+
+# Full Deviance
+m1_yp <- outm1$mean$y2
+m1_yt <- c(dat1$cawadet[test.eBird], dat1$cawatot[test.BBA], dat1$cawadet[test.BBS])
+m1_yt <- cbind(m1_yt, c(1 - dat1$cawadet[test.eBird], 5 - dat1$cawatot[test.BBA], 1 - dat1$cawadet[test.BBS]))
+m1_yp[9218:15947] <- m1_yp[9218:15947]/5
+m1_yp <- 0.0001 + m1_yp*0.9998
+m1_dev <- -2*sum(log((m1_yp^m1_yt[,1])*((1-m1_yp)^(m1_yt[,2]))))
+#eBird deviance
+Em1_yp <- outm1$mean$y2[1:9217]
+Em1_yt <- c(dat1$cawadet[test.eBird])
+Em1_yt <- cbind(Em1_yt, c(1 - dat1$cawadet[test.eBird]))
+Em1_yp <- 0.0001 + Em1_yp*0.9998
+Em1_dev <- -2*sum(log((Em1_yp^Em1_yt[,1])*((1-Em1_yp)^(Em1_yt[,2]))))
+#BBA deviance
+Am1_yp <- outm1$mean$y2[9218:15947]
+Am1_yt <- c(dat1$cawatot[test.BBA])
+Am1_yt <- cbind(Am1_yt, c(5 - dat1$cawatot[test.BBA]))
+Am1_yp <- Am1_yp/5
+Am1_yp <- 0.0001 + Am1_yp*0.9998
+Am1_dev <- -2*sum(log((Am1_yp^Am1_yt[,1])*((1-Am1_yp)^(Am1_yt[,2]))))
+#BBS deviance
+Sm1_yp <- outm1$mean$y2[15948:24220]
+Sm1_yt <- c(dat1$cawadet[test.BBS])
+Sm1_yt <- cbind(Sm1_yt, c(1 - dat1$cawadet[test.BBS]))
+Sm1_yp <- 0.0001 + Sm1_yp*0.9998
+Sm1_dev <- -2*sum(log((Sm1_yp^Sm1_yt[,1])*((1-Sm1_yp)^(Sm1_yt[,2]))))
+
+
+# Brier score
+brier1 <- mean((outm1$mean$y2[9218:24220] - dat1$cawadet[88727:103729])^2)
+
+
+# AUC
+pred1 <- prediction(as.numeric(outm1$mean$y2[9218:24220]), dat1$cawadet[88727:103729])
+auc1 <- performance(pred1, measure = "auc")
+auc1 <- auc1@y.values[[1]]
+
+# roc1 <- roc(dat1$cawadet[88642:103748], as.numeric(outm1$mean$y2[9218:24220]),
+# ci.auc=TRUE, auc=TRUE, parallel=TRUE)
+
+
+# Save CAWA M1 file
+save(outm1, m1, datm1, m1_dev, Em1_dev, Am1_dev, Sm1_dev,
+     brier1, pred1, auc1,
+     file = paste0(path,"results/out/cawam1.RData"))
+
+
+### GAMs --------------------------------------------------------------------
+
+#Sim2Jam
+require(rjags)
+# load("results/out/cawam1.RData")
+
+jm1 <- jags.model(paste0(path,"models/cawam1.txt"), 
+                  data=datm1, 
+                  inits=initsm1, 
+                  n.adapt=500, 
+                  n.chains=3)
+update(jm1, 500)
+
+sam1 <- jags.samples(jm1, c("b","rho"), n.iter=2000, thin=2)
+jam1 <- sim2jam(sam1, m1$pregam)
+
+save(m1, datm1, jm1, sam1, jam1,
+     file = paste0(path,"results/jams/cawam1jam.RData"))
+
+# Base plotting of GAMs
+# pdf("FinalResults/Plots/cawam1cov.pdf")
+# plot(jam1, pages=1)
+# dev.off()
+
+
+# End script
---
+++ code/cawa11-wip.R
@@ -0,0 +1,149 @@
+
+## This code was written by: fiona lunt
+
+## Objective ---------------------------
+## Model 11: F4 (stationary shared, no travelling)
+## Canada warbler (CAWA) 
+##
+## Input:
+##    covdat.csv
+##
+## Output: 
+##    cawam11.RData
+##
+
+## load packages ---------------------------
+library(plyr)
+library(magrittr)
+library(dplyr)
+library(tibble)
+library(mgcv)
+library(jagsUI)
+library(ROCR)
+
+
+## load functions ---------------------------
+source('functions.R')
+
+## load data ---------------------------
+covdat2 <- read.csv("data/covdat2.csv")
+
+
+
+# Model 11: F4 - CAWA -----------------------------------------------------
+#train: eBird stat- 1:14725, BBA- 14726:41728, BBS- 41729:74405
+#test: eBird- 74406:83622, BBA- 83623:90352, BBS- 90353:98625
+
+m11 <- generate.code(covdat2)
+
+datm11 <- list(y = c(covdat2$cawadet[1:14725], covdat2$cawatot[14726:41728], covdat2$cawadet[41729:74405]), 
+               X = m11$jags.data$X, n = m11$jags.data$n, zero = m11$jags.data$zero,
+               S1 = m11$jags.data$S1, S2 = m11$jags.data$S2, S3 = m11$jags.data$S3,
+               S4 = m11$jags.data$S4, S5 = m11$jags.data$S5, S6 = m11$jags.data$S6,
+               S7 = m11$jags.data$S7, S8 = m11$jags.data$S8, S9 = m11$jags.data$S9,
+               ehours = covdat2$duration_minutes[1:14725], 
+               ehours2 = covdat2$duration_minutes[74406:83622], 
+               hsm = covdat2$hsm[14726:41728],
+               hsm2 = covdat2$hsm[83623:90352], 
+               doy = covdat2$doy[14726:41728],
+               doy2 = covdat2$doy[83623:90352],
+               doy3 = covdat2$doy[41729:74405],
+               doy4 = covdat2$doy[90353:98625])
+
+initsm11 <- function(){
+  list(b = m11$jags.ini$b, 
+       lambda = m11$jags.ini$lambda,  
+       beta = rep(3, 0.001), 
+       z = as.numeric(datm11$y>0))
+}
+
+outm11 <- jags(data = datm11, parameters.to.save = c("beta","b","y2"), inits = initsm11, 
+               model.file = "FinalModels/cawam11.txt", 
+               n.chains = 3, n.thin = 2, n.adapt = 500, n.burnin = 500, n.iter = 2500, parallel=TRUE)
+
+#Full Deviance
+m11_yp <- outm11$mean$y2
+m11_yt <- c(covdat2$cawadet[74406:83622], covdat2$cawatot[83623:90352], covdat2$cawadet[90353:98625])
+m11_yt <- cbind(m11_yt, c(1 - covdat2$cawadet[74406:83622], 5 - covdat2$cawatot[83623:90352], 1 - covdat2$cawadet[90353:98625]))
+m11_yp[9218:15947] <- m11_yp[9218:15947]/5
+m11_yp <- 0.0001 + m11_yp*0.9998
+m11_dev <- -2*sum(log((m11_yp^m11_yt[,1])*((1-m11_yp)^(m11_yt[,2]))))
+#eBird deviance
+Em11_yp <- outm11$mean$y2[1:9217]
+Em11_yt <- c(covdat2$cawadet[74406:83622])
+Em11_yt <- cbind(Em11_yt, c(1 - covdat2$cawadet[74406:83622]))
+Em11_yp <- 0.0001 + Em11_yp*0.9998
+Em11_dev <- -2*sum(log((Em11_yp^Em11_yt[,1])*((1-Em11_yp)^(Em11_yt[,2]))))
+#BBA deviance
+Am11_yp <- outm11$mean$y2[9218:15947]
+Am11_yt <- c(covdat2$cawatot[83623:90352])
+Am11_yt <- cbind(Am11_yt, c(5 - covdat2$cawatot[83623:90352])) 
+Am11_yp <- Am11_yp/5
+Am11_yp <- 0.0001 + Am11_yp*0.9998
+Am11_dev <- -2*sum(log((Am11_yp^Am11_yt[,1])*((1-Am11_yp)^(Am11_yt[,2]))))
+#BBS deviance
+Sm11_yp <- outm11$mean$y2[15948:24220]
+Sm11_yt <- c(covdat2$cawadet[90353:98625])
+Sm11_yt <- cbind(Sm11_yt, c(1 - covdat2$cawadet[90353:98625]))
+Sm11_yp <- 0.0001 + Sm11_yp*0.9998
+Sm11_dev <- -2*sum(log((Sm11_yp^Sm11_yt[,1])*((1-Sm11_yp)^(Sm11_yt[,2]))))
+
+save(outm11, m11, m11_dev, Em11_dev, Am11_dev, Sm11_dev, 
+     file = "FinalResults/cawam11_2.RData")
+
+brier11 <- mean((outm11$mean$y2[9218:24220] - covdat2$cawadet[83623:98625])^2)
+
+pred11 <- prediction(as.numeric(outm11$mean$y2[9218:24220]), covdat2$cawadet[83623:98625])
+auc11 <- performance(pred11, measure = "auc")
+auc11 <- auc11@y.values[[1]]
+
+
+
+#################################
+#Sim2Jam, model 11 (aka 10)
+require(rjags)
+jm11 <- jags.model("FinalModels/cawam11.txt", data=datm11, inits=initsm11, n.adapt=500, n.chains=3)
+update(jm11, 500)
+sam11 <- jags.samples(jm11, c("b","rho"), n.iter=2000, thin=2)
+jam11 <- sim2jam(sam11, m11$pregam)
+
+save(m11, jm11, sam11, jam11,
+     file = "FinalResults/Plots/cawam11_jam.RData")
+
+pdf("FinalResults/Plots/cawa11cov.pdf")
+plot(jam11, pages=1)
+dev.off()
+
+load("S:/MillerLab/Projects/BirdSDM/FinalResults/Plots/grid_pa_1km.RData")
+library(ggplot2)
+library(sf)
+library(sp)
+library(RColorBrewer)
+
+#Generate predictions
+grid3$pred <- predict(jam11, newdata = grid3, type="response")
+grid_sp <- as_Spatial(grid3)
+
+#Change map extent
+scale.parameter = 1.1
+original.bbox = grid_sp@bbox 
+edges = original.bbox
+edges[1, ] <- (edges[1, ] - mean(edges[1, ])) * scale.parameter + mean(edges[1, 
+])
+edges[2, ] <- (edges[2, ] - mean(edges[2, ])) * scale.parameter + mean(edges[2, 
+])
+
+#Plot
+pdf("FinalResults/Plots/cawa11map.pdf")
+spplot(grid_sp, "pred", col=NA, par.settings = list(axis.line = list(col = 'transparent')),
+       xlim = edges[1, ], ylim = edges[2, ], colorkey=list(height=0.6, cex=0.5))
+dev.off()
+
+
+
+
+
+###spplot(grid_sp, "pred", col=NA, par.settings = list(axis.line = list(col = 'transparent')),
+# xlim = edges[1, ], ylim = edges[2, ], at = seq(0, 0.4, 0.025), 
+# colorkey=list(labels=list(labels=(seq(0, 0.4, 0.05))), height=0.6, cex=0.5))
+
