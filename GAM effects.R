23a19d63a16ac4ca222c0d7a37612bb14eae7a43
Riley Mummah <rmummah@umass.edu>
2024-02-05 08:14:04 -0500

    Updated output

--- GAM effects.R
+++ GAM effects.R
@@ -1,68 +1,81 @@
-## FROM CAWA1.R
-# jm1 <- jags.model("FinalModels/cawam1.txt", data=datm1, inits=initsm1, n.adapt=500, n.chains=3)
-# update(jm1, 500)
-# sam1 <- jags.samples(jm1, c("b","rho"), n.iter=2000, thin=2)
-# jam1 <- sim2jam(sam1, m1$pregam)
-# 
-# save(m1, jm1, sam1, jam1,
-#      file = "FinalResults/Jams/cawam1jam.RData")
-# 
-# # pdf("FinalResults/Plots/cawam1cov.pdf")
-# plot(jam1, pages=1)
-# # dev.off()
+library(mgcv)
+library(ROCR)
+library(dplyr)
 
 
-load("results/jams/cawam1jam.RData")
-load("results/jams/cawam2jam.RData")
+# Define set up spp and models
+files <- data <- expand.grid(spp = c(#'cawa',
+  'cerw','gwwa','woth'),
+                             mod = c(1:8, 10:16))
 
-tmp <- plot(jam2, pages=1)
 
-# set up empty dataframe
-dat <- data.frame(cov=NA,
-                  x=NA,
-                  fit=NA,
-                  se=NA)
-
-# Collect all but lat/long, which is index 7
-for (k in 1:6) {
-  dat <- bind_rows(dat, data.frame(cov=tmp[[k]]$xlab,
-                                   x=tmp[[k]]$x,
-                                   fit=tmp[[k]]$fit[,1],
-                                   se=tmp[[k]]$se,
-                                   mod='F1'))
+for (i in 1:nrow(files)) {
+  # load data [j, sam, jam]
+  load(paste0("results/jams/",files$spp[i],"m",files$mod[i],"jam.RData"))
+  
+  # set up empty dataframe
+  dat <- data.frame(cov=NA,
+                    x=NA,
+                    fit=NA,
+                    se=NA,
+                    mod=NA,
+                    spp=NA)
+  
+  # Plot jam to extract estimates
+  tmp <- plot(jam, pages=1)
+  
+  
+  # Collect all but lat/long, which is index 7
+  for (k in 1:6) {
+    dat <- bind_rows(dat, data.frame(cov=tmp[[k]]$xlab,
+                                     x=tmp[[k]]$x,
+                                     fit=tmp[[k]]$fit[,1],
+                                     se=tmp[[k]]$se,
+                                     mod=files$mod[i],
+                                     spp=files$spp[i]))
+  }
+  
+  # WIP for plotting lat/long effects
+  # data.frame(x=tmp[[7]]$x,
+  #            y=tmp[[7]]$fit[,1]) %>% view()
+  #   ggplot() + geom_line(aes(x=x,y=y))
+  
+  dat %>%
+    # Remove row of NA
+    slice(-1) %>%
+    full_join(data,.) -> data
+  
+  rm(list = ls()[!ls() %in% c('data','files','i')])
+  
+  print(paste0('Model ', files$mod[i], ' from ', files$spp[i],' is complete.'))
+    
 }
 
-# WIP for plotting lat/long effects
-data.frame(x=tmp[[7]]$x,
-           y=tmp[[7]]$fit[,1]) %>% view()
-  ggplot() + geom_line(aes(x=x,y=y))
+data %>%
+  filter(!is.na(cov)) -> data
+
+saveRDS(data, 'results/gams.rds')
 
 
-dat %>%
-  slice(-1) %>%
-ggplot(aes(x=x)) +
-  geom_ribbon(aes(ymin=fit-se, ymax=fit+se, group=mod, fill=mod),
-              alpha=0.5) + 
-  geom_line(aes(y=fit, group=mod, col=mod)) +
-  facet_wrap(~cov, scales = 'free') +
+data <- readRDS('results/gams.rds')
+
+mod.names <- data.frame(num = c(1,2,3,4,5,6,7,8,10,11,12,13,14,15,16),
+                        name = c("R1","F1","F2","F3","F5","F6","E1","E2","E3","F4","R2","O3","O2","O1","E4"))
+
+# left_join(data, mod.names, by = c('mod'='num')) %>%
+#   mutate(group = case_when(grep('F',name) ~ 'F',
+#                            grep('E',name) ~ 'E',
+#                            grep('O',name) ~ 'O',
+#                            .default = 'R')) -> data
+
+ggplot(data, aes(x=x)) +
+  geom_ribbon(aes(ymin=fit-se, ymax=fit+se,
+                  group=mod, fill=as.factor(mod)),
+              alpha=0.25) +
+  geom_line(aes(y=fit, group=mod, col=as.factor(mod))) +
+  facet_grid(spp~cov, scales = 'free') +
   labs(x='')
 
-
-load("results/out/cawam1.RData")
-load("results/out/cawam2.RData")
-load("results/out/cawam3.RData")
-
-data.frame(brier = c(brier2-brier1, brier3-brier1)/brier1,
-           dev = c(m2_dev-m1_dev, m3_dev-m1_dev)/m1_dev,
-           auc = c(auc2-auc1, auc3-auc1)/auc1,
-           mod = c('F1','F2')) %>% 
-  pivot_longer(cols=1:3,
-               names_to = 'metric',
-               values_to = 'value') %>%
-  ggplot() +
-  geom_hline(aes(yintercept = 0), col='black') +
-  geom_line(aes(x=mod, y=value, group=metric, col=metric))
-  # facet_wrap(~)
 
 
 
