cec34903f3b8810a3b0792ae8c41bc4f2d5e0fe5
lanescher <clanescher@gmail.com>
2024-02-06 11:37:39 -0500

    figures

--- code/observations.R
+++ code/observations.R
@@ -1,64 +1,351 @@
 library(tidyverse)
-
-dat <- read.csv("../species-futures/data/proj05-fiona/observations.csv")
-
-dat$code <- factor(dat$code, levels = c("R1", "F1", "F2", "F3", "F4", "E1", "E2",
-                                        "I1", "I2", "I3", "R2", "O1", "O2", "O3"))
+library(plyr)
 
 
 
-# total observations
-dat1 <- dat %>%
-  select(model, code, eBird, BBA, BBS) %>%
-  pivot_longer(!c("model", "code"))
+summarize_obs <- function(df, mod) {
+  
+  obs <- as.data.frame(matrix(nrow = 5, ncol = 6))
+  colnames(obs) <- c("model", "group", "eBird", "BBS", "BBA", "all")
+  
+  
+  ebd <- df %>% filter(is.na(checklist_id) == F)
+  bbs <- df %>% filter(is.na(Route) == F)
+  bba <- df %>% filter(is.na(BBA_ID) == F)
+  
+  # fill in numb obs
+  obs[1, 1] <- mod
+  obs[1, 2] <- "total"
+  obs[1, 3] <- nrow(ebd)
+  obs[1, 4] <- nrow(bbs)
+  obs[1, 5] <- nrow(bba)
+  obs[1, 6] <- nrow(df)
+  
+  # fill in numb cawa
+  obs[2, 1] <- mod
+  obs[2, 2] <- "cawa"
+  obs[2, 3] <- nrow(ebd[which(ebd$cawadet == 1),])/ nrow(ebd)
+  obs[2, 4] <- nrow(bbs[which(bbs$cawadet == 1),])/ nrow(bbs)
+  obs[2, 5] <- nrow(bba[which(bba$cawatot == 1),])/ nrow(bba)
+  obs[2, 6] <- (nrow(ebd[which(ebd$cawadet == 1),]) + nrow(bbs[which(bbs$cawadet == 1),]) + nrow(bba[which(bba$cawatot == 1),]))/ nrow(df)
+  
+  # fill in numb cerw
+  obs[3, 1] <- mod
+  obs[3, 2] <- "cerw"
+  obs[3, 3] <- nrow(ebd[which(ebd$cerwdet == 1),])/ nrow(ebd)
+  obs[3, 4] <- nrow(bbs[which(bbs$cerwdet == 1),])/ nrow(bbs)
+  obs[3, 5] <- nrow(bba[which(bba$cerwtot == 1),])/ nrow(bba)
+  obs[3, 6] <- (nrow(ebd[which(ebd$cerwdet == 1),]) + nrow(bbs[which(bbs$cerwdet == 1),]) + nrow(bba[which(bba$cerwtot == 1),]))/ nrow(df)
+  
+  # fill in numb gwwa
+  obs[4, 1] <- mod
+  obs[4, 2] <- "gwwa"
+  obs[4, 3] <- nrow(ebd[which(ebd$gwwadet == 1),])/ nrow(ebd)
+  obs[4, 4] <- nrow(bbs[which(bbs$gwwadet == 1),])/ nrow(bbs)
+  obs[4, 5] <- nrow(bba[which(bba$gwwatot == 1),])/ nrow(bba)
+  obs[4, 6] <- (nrow(ebd[which(ebd$gwwadet == 1),]) + nrow(bbs[which(bbs$gwwadet == 1),]) + nrow(bba[which(bba$gwwatot == 1),]))/ nrow(df)
+  
+  # fill in numb woth
+  obs[5, 1] <- mod
+  obs[5, 2] <- "woth"
+  obs[5, 3] <- nrow(ebd[which(ebd$wothdet == 1),])/ nrow(ebd)
+  obs[5, 4] <- nrow(bbs[which(bbs$wothdet == 1),])/ nrow(bbs)
+  obs[5, 5] <- nrow(bba[which(bba$wothtot == 1),])/ nrow(bba)
+  obs[5, 6] <- (nrow(ebd[which(ebd$wothdet == 1),]) + nrow(bbs[which(bbs$wothdet == 1),]) + nrow(bba[which(bba$wothtot == 1),]))/ nrow(df)
+  
+  
+  return(obs)
+}
 
-dat1a <- dat1 %>%
-  group_by(code) %>%
-  summarize(total = sum(value)) %>%
-  inner_join(dat1, by = "code") %>%
-  mutate(prop = value/total)
+
+## pull number of observations and number of detections from each file
 
 
+dat <- c()
+
+
+
+
+# File 1, R1 ----
+
+mod <- "R1"
+
+
+#test1 <- read.csv("data/test1.csv")
+train1 <- read.csv("data/train1.csv")
+
+#dat1 <- rbind.fill(train1, test1)
+
+# we're only counting observations used to train the model
+obs <- summarize_obs(train1, mod = mod)
+
+dat <- bind_rows(dat, obs)
+
+
+
+# File 2, F1 ----
+
+mod <- "F1"
+
+#test1 <- read.csv("data/test1.csv")
+train2 <- read.csv("data/train2.csv")
+
+#dat2 <- rbind.fill(train2, test1)
+
+obs <- summarize_obs(train2, mod = mod)
+
+dat <- bind_rows(dat, obs)
+
+
+
+# File 3, F2 ----
+
+mod <- "F2"
+
+#test1 <- read.csv("data/test1.csv")
+train3 <- read.csv("data/train3.csv")
+
+#dat3 <- rbind.fill(train3, test1)
+
+obs <- summarize_obs(train3, mod = mod)
+
+dat <- bind_rows(dat, obs)
+
+
+
+
+# File 4, F3 ----
+
+mod <- "F3"
+
+#test1 <- read.csv("data/test1.csv")
+train4 <- read.csv("data/train4.csv")
+
+#dat4 <- rbind.fill(train4, test1)
+
+obs <- summarize_obs(train4, mod = mod)
+
+dat <- bind_rows(dat, obs)
+
+
+
+# File 5, F5 ----
+
+mod <- "F5"
+
+#test1 <- read.csv("data/test1.csv")
+train1 <- read.csv("data/train1.csv")
+
+#dat1 <- rbind.fill(train1, test1)
+
+obs <- summarize_obs(train1, mod = mod)
+
+
+dat <- bind_rows(dat, obs)
+
+
+
+
+# File 6, F6 ----
+
+mod <- "F6"
+
+#test1 <- read.csv("data/test1.csv")
+train4 <- read.csv("data/train4.csv")
+
+#dat4 <- rbind.fill(train4, test1)
+
+obs <- summarize_obs(train4, mod = mod)
+
+dat <- bind_rows(dat, obs)
+
+
+
+# File 7, E1 ----
+
+mod <- "E1"
+
+#test1 <- read.csv("data/test1.csv")
+train1 <- read.csv("data/train1.csv")
+
+#dat1 <- rbind.fill(train1, test1)
+
+obs <- summarize_obs(train1, mod = mod)
+
+
+dat <- bind_rows(dat, obs)
+
+
+# File 8, E2 ----
+
+mod <- "E2"
+
+covdat <- read.csv("data/covdat.csv")
+#training data ends at 177616
+covdat <- covdat[1:177616,]
+
+# this model doesn't use ebird observations at all
+covdat <- filter(covdat, is.na(checklist_id) == T)
+
+obs <- summarize_obs(covdat, mod = mod)
+
+dat <- bind_rows(dat, obs)
+
+
+# File 10, E3 ----
+
+mod <- "E3"
+
+covdat <- read.csv("data/covdat.csv")
+
+#training data ends at 177616
+covdat <- covdat[1:177616,]
+
+# this model doesn't use ebird observations at all
+covdat <- filter(covdat, is.na(checklist_id) == T)
+
+
+obs <- summarize_obs(covdat, mod = mod)
+
+
+dat <- bind_rows(dat, obs)
+
+
+# File 11, F4 ----
+
+mod <- "F4"
+
+covdat2 <- read.csv("data/covdat2.csv")
+
+#training data ends at 74405
+covdat2 <- covdat2[1:74405,]
+
+# this model doesn't use traveling ebird checklists
+covdat2 <- filter(covdat2, protocol_type == "Stationary" | is.na(protocol_type))
+
+
+obs <- summarize_obs(covdat2, mod = mod)
+
+dat <- bind_rows(dat, obs)
+
+
+# File 12, R2 ----
+
+mod <- "R2"
+
+train1 <- read.csv("data/train1.csv")
+#test2 <- read.csv("data/test2.csv")
+
+#dat12 <- rbind.fill(train1, test2)
+
+obs <- summarize_obs(train1, mod = mod)
+
+
+dat <- bind_rows(dat, obs)
+
+
+
+# File 13, O3 ----
+
+mod <- "O3"
+
+hdat <- read.csv("data/hdat.csv")
+# create BBA_ID column so that function works. It'll all be NAs because this model doesnt use BBA
+hdat$BBA_ID <- NA
+
+#training data ends at 19556
+hdat <- hdat[1:19556,]
+
+
+obs <- summarize_obs(hdat, mod = mod)
+
+dat <- bind_rows(dat, obs)
+
+
+# File 14, O2 ----
+
+mod <- "O2"
+
+hdat <- read.csv("data/hdat.csv")
+# create BBA_ID column so that function works. It'll all be NAs because this model doesnt use BBA
+hdat$BBA_ID <- NA
+
+#training data ends at 19556
+hdat <- hdat[1:19556,]
+
+obs <- summarize_obs(hdat, mod = mod)
+
+
+dat <- bind_rows(dat, obs)
+
+
+# File 15, O1 ----
+
+mod <- "O1"
+
+train1 <- read.csv("data/train1.csv")
+#test2 <- read.csv("data/test2.csv")
+
+#dat12 <- rbind.fill(train1, test2)
+
+obs <- summarize_obs(train1, mod = mod)
+
+dat <- bind_rows(dat, obs)
+
+
+# File 16, E4 ----
+
+mod <- "E4"
+
+#test1 <- read.csv("data/test1.csv")
+train1 <- read.csv("data/train1.csv")
+
+#dat1 <- rbind.fill(train1, test1)
+
+obs <- summarize_obs(train1, mod = mod)
+
+
+dat <- bind_rows(dat, obs)
+
+
+
+dat.pl <- pivot_longer(dat, !c("model", "group"))
+dat.pl$value[which(is.na(dat.pl$value))] <- 0
+
+dat.pl$model <- factor(dat.pl$model, levels = c("R1", "F1", "F2", "F3", "F4", "F5", "F6",
+                                        "E1", "E2", "E3", "E4", "R2", "O1", "O2", "O3"))
+
+dat.pl$name <- factor(dat.pl$name, levels = c("eBird", "BBA", "BBS"))
+
+spps <- c("Canada Warbler", "Cerulean Warbler", "Golden-winged Warbler", "Wood Thrush")
+names(spps) <- c("cawa", "cerw", "gwwa", "woth")
 
 
 cols <- c("#D81B60", "#1E88E5", "#FFC107")
 
 
-ggplot(dat1) +
+ggplot(filter(dat.pl, group == "total", name != "all")) +
   geom_hline(yintercept = 0) +
-  geom_bar(aes(x = code, y = value, fill = name, color = name), stat = "identity", position = "stack") +
+  geom_bar(aes(x = model, y = value, fill = name, color = name), stat = "identity", position = "stack") +
   scale_fill_manual(values = colorspace::lighten(cols, .1)) +
   scale_color_manual(values = colorspace::darken(cols, .4)) +
   theme_bw() +
-  labs(x = "Model", y = "Total observations", fill = "Data \nsource", color = "Data \nsource")
-
-ggsave("outputs/proj05-fiona/figures/totalobservations.jpg", height = 6, width = 9)
-
-
-# detections
-
-dat2 <- dat %>%
-  #select(model, code, Total, Canada.Warbler, Cerulean.Warbler, Golden.winged.Warbler, Wood.Thrush) %>%
-  mutate(CAWA = Canada.Warbler/Total,
-         CEWA = Cerulean.Warbler/Total,
-         GWWA = Golden.winged.Warbler/Total,
-         WOTH = Wood.Thrush/Total) %>%
-  select(model, code, CAWA, CEWA, GWWA, WOTH) %>%
-  pivot_longer(!c("model", "code")) %>%
-  mutate(perc = value * 100)
-
-labs <- c("Canada Warbler", "Cerulean Warbler", "Golden-winged Warbler", "Wood Thrush")
-names(labs) <- c("CAWA", "CEWA", "GWWA", "WOTH")
+  labs(x = "Model", y = "Number of observations", fill = "Data \nsource", color = "Data \nsource")
+ggsave(file = "outputs/observations.jpg", height = 5, width = 10)
 
 
-ggplot(dat2) +
+
+ggplot(filter(dat.pl, group != "total", name != "all", value > 0)) +
   geom_hline(yintercept = 0) +
-  geom_bar(aes(x = code, y = value), stat = "identity", position = "stack",
-           fill = "gray80", color = "gray20") +
-  facet_wrap(~name, scales = "free_y", labeller = labeller(name = labs)) +
+  geom_point(aes(x = model, y = value, color = name)) +
+  scale_color_manual(values = colorspace::lighten(cols, .1)) +
   theme_bw() +
-  theme(strip.background = element_blank()) +
-  labs(x = "Model", y = "Detection rate")
+  labs(x = "Model", y = "Detection rate", fill = "Data \nsource", color = "Data \nsource") +
+  facet_wrap(~ group, scales = "free", labeller = labeller(group = spps))
+ggsave(file = "outputs/detection-rate.jpg", height = 5, width = 10)
 
 
-ggsave("outputs/proj05-fiona/figures/detections.jpg", height = 6, width = 9)
+
+
+
+
---
+++ code/plot-parameter-estimates.R
@@ -0,0 +1,91 @@
+library(tidyverse)
+library(mgcv)
+
+
+
+gam <- readRDS("results/gams.rds")
+
+
+# Partial figure (for text)
+
+labs <- c("Elevation", "Slope", "Temperature", "Precipitation", "Canopy cover", "Developed land", "Forested land", "Road length")
+names(labs) <- c("elev", "slope", "temp", "ppt", "can", "dev", "forest", "road")
+
+spps <- c("Canada Warbler", "Cerulean Warbler", "Golden-winged Warbler", "Wood Thrush")
+names(spps) <- c("cawa", "cerw", "gwwa", "woth")
+
+gam <- gam %>%
+  mutate(species = case_when(spp == "cawa" ~ "Canada Warbler",
+                             spp == "cerw" ~ "Cerulean Warbler",
+                             spp == "gwwa" ~ "Golden-winged Warbler",
+                             spp == "woth" ~ "Wood Thrush"))
+
+ggplot(data = filter(gam, name == "R1"), aes(x = x)) +
+  geom_hline(yintercept = 0) +
+  geom_ribbon(aes(ymin = fit-se, ymax = fit+se, group = species, fill = species),
+              alpha=0.3) + 
+  geom_line(aes(y=fit, group = species, col = species)) +
+  facet_wrap(~cov, labeller = labeller(cov = labs)) +
+  labs(x = 'Covariate value', y = "Estimate", fill = "Species", color = "Species") +
+  theme_bw() +
+  theme(strip.background = element_blank()) +
+  scale_color_manual(values = c("Golden-winged Warbler" = "gold1", 
+                                "Wood Thrush" = "darkorange3", 
+                                "Cerulean Warbler" = "steelblue1", 
+                                "Canada Warbler" = "gray40")) +
+  scale_fill_manual(values = c("Golden-winged Warbler" = "gold1", 
+                                "Wood Thrush" = "darkorange3", 
+                                "Cerulean Warbler" = "steelblue1", 
+                               "Canada Warbler" = "gray40"))
+
+ggsave(file = "outputs/parameter-partial.jpg", height = 5, width = 10)
+
+
+
+
+
+
+# Full figure (for supplement)
+
+labs <- c("Elevation", "Slope", "Temperature", "Precipitation", "Canopy cover", "Developed land", "Forested land", "Road length")
+names(labs) <- c("elev", "slope", "temp", "ppt", "can", "dev", "forest", "road")
+
+spps <- c("Canada Warbler", "Cerulean Warbler", "Golden-winged Warbler", "Wood Thrush")
+names(spps) <- c("cawa", "cerw", "gwwa", "woth")
+
+ggplot(data = gam, aes(x = x)) +
+  geom_hline(yintercept = 0) +
+  geom_ribbon(aes(ymin = fit-se, ymax = fit+se, group = interaction(spp, name), fill = as.factor(name)),
+              alpha=0.05) + 
+  geom_line(aes(y=fit, group = interaction(spp, name), col = as.factor(name))) +
+  # facet_grid(rows = vars(cov), cols = vars(spp), 
+  #            labeller = labeller(cov = labs, spp = spps),
+  #            scales = "free") +
+  facet_grid(spp ~ cov, scales = "free",
+             labeller = labeller(cov = labs, spp = spps)) +
+  labs(x = 'Covariate value', y = "Estimate", fill = "Model", color = "Model") +
+  theme_bw() +
+  theme(strip.background = element_blank())
+
+ggsave(file = "outputs/parameter-full.jpg", height = 12, width = 12)
+
+
+
+# Figure out which ones are way different
+
+# there are three blue-green lines that are very different. They all have very low
+# y values for GWWA elevation
+tmp <- gam %>%
+  filter(spp == "gwwa", cov == "elev") %>%
+  group_by(name) %>%
+  summarize(low = min(fit))
+# F2, F3, and F6 have very different values
+
+
+# there are two orange lines that are slightly different. They have high y values
+# at high x values for CAWA forest
+tmp <- gam %>%
+  filter(spp == "cawa", cov == "forest") %>%
+  group_by(name) %>%
+  summarize(high = max(fit))
+# E2 and E3 have slightly different values
